cf6d842d18a1584b2c607186c1378855087c2e88
ShinyaKobukai <42952490+ShinyaKobukai@users.noreply.github.com>
2020-01-22 14:47:09 +0900

    update to js and db cntrl

--- edit/profile_edit.php
+++ edit/profile_edit.php
@@ -1,34 +1,28 @@
 <?php
   include_once("../common/db_connect.php");
-  $pdo = db_connect();
   session_start();
   $user_id = $_SESSION['user_id'];
-  if(isset($_POST['edit'])){
-      if ( empty($_POST['user_name']) ) {
-        $_SESSION["msg"] = '入力してください';
-        print("入力してください");
-        header('Location: profile_edit.html');
-        exit;
+  foreach($_POST as $key => $val){
+    $$key=trim(htmlspecialchars($val)); 
+  }
+  try {
+      $pdo = db_connect();
+      $sql = $pdo->prepare(
+        " SELECT
+          user_name
+          FROM
+          user
+          WHERE
+          user_id = :user_id "
+        );
+      $sql->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+      $sql->execute();
+    } catch (PDOException $e) {
+      exit('データベース接続失敗。'.$e->getMessage());
     }
-    foreach($_POST as $key => $val){
-      $$key=trim(htmlspecialchars($val)); 
-    }
-          $pdo = db_connect();
-          $sql = "UPDATE user SET user_name = ?, icon = ? WHERE user_id = ?";
-          $stmt = $pdo->prepare($sql);
-            $stmt->execute(array($user_name,$user_icon,$user_id));
-
-          if (empty($user_icon)) {
-            $_SESSION["msg"] = 'アイコンが設定されませんでした。';
-            header('Location: profile_edit.php');
-              exit;
-          }
-
-        } catch (PDOException $e) {
-          exit('データベース接続失敗。'.$e->getMessage());
-        }
-        header('Location: ../post_list.php');
-      }
+    $row = $sql->fetch();
+    $user_name = $row['user_name'];
+    var_dump($user_name);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -57,58 +51,36 @@
 <main>
   <div class="regi_info">
     <h1>プロフィール設定</h1>
-    <form action="profile_edit.php" name="form1" method="POST" id = "form" enctype="multipart/form-data">
+    <form action="profile_proce.php" name="form1" method="post" enctype=multipart/form-data>
       <label for="box">新しいユーザー名</label>
-      <input type="text" name="user_name" value="" id="user_name"/>
+      <input type="text" name="user_name" value="<?php echo $user_name ?>" id="user_name"/>
       <label for="box">新しいユーザーアイコン</label>
       <input name="user_icon" type="file" id="user_icon">
-      <input type="button" name="edit" value="変更" onclick="clickBtn();" />
+      <input type="submit" value="変更" id="btn" />    
     </form>
   </div>
+  <script src="../js/all.js"></script>
+  <script>
+  (function(){
+    document.querySelector('#btn').onclick = function(){
+      var user_name = document.form1.user_name.value;
+      var user_icon = document.form1.user_icon.value;
+      document.querySelector('#user_name').textContent = user_name;
+      if(document.querySelector('#user_icon').value == null && document.querySelector('#user_name').value == null){
+        alert('変更内容が存在しません');
+        return false;
+      }
+      if (!user_name.match(/\S/g)){
+        alert('記述に問題があります。訂正してください。');
+        return false;
+      } 
+    }
+  })();
+  </script>
+
 </main>
   <footer>
     <address>&copy;2019 buono All Rights Reserved.</address>
   </footer>
-  <script src="../js/all.js">
-    var clickBtn = function(){
-      var user_name = document.form1.user_name.value;
-      var user_icon = document.form1.user_icon.value;
-      document.querySelector('#user_name').textContent = user_name;
-      if (!user_name.match(/\S/g)){
-        alert('記述に問題があります。訂正してください。');
-      } else if(document.querySelector('#user_icon').value!=null){
-        <?php
-          $user_icon = file_get_contents($_FILES["user_icon"]["tmp_name"]);
-          $user_icon = str_replace("data:image/jpeg;base64,","",$user_icon);
-          $user_icon = base64_encode($user_icon);
-          try {
-              $pdo = db_connect();
-              $sql = "UPDATE user SET user_name = ?, icon = ? WHERE user_id = ?";
-              $stmt = $pdo->prepare($sql);
-                $stmt->execute(array($user_name,$user_icon,$user_id));
-
-              if (empty($user_icon)) {
-                $_SESSION["msg"] = 'アイコンが設定されませんでした。';
-                header('Location: profile_edit.php');
-                  exit;
-              }
-
-            } catch (PDOException $e) {
-              exit('データベース接続失敗。'.$e->getMessage());
-            }
-            header('Location: ../post_list.php');
-          ?>
-        }
-      }
-      post_edit.trim();
-      alert('編集が完了しました。'+post_edit);
-      if (window.confirm("ポストリストに戻りますか？")) { 
-        var pram = "/buono/edit/edit_result.php?post_id="+post_id+"&amp;post_edit="+post_edit;
-        location.href = pram;
-      }
-      console.log(post_edit);
-    }
-  }
-  </script>
 </body>
 </html>
--- edit/profile_proce.php
+++ edit/profile_proce.php
@@ -1,37 +1,40 @@
-<?php
+<?php 
 	include_once("../common/db_connect.php");
 	session_start();
-	$user_id = $_SESSION['user_id'];
-	if(isset($_POST['edit'])){
-   		if ( empty($_POST['user_name']) ) {
-     		$_SESSION["msg"] = '入力してください';
-     		print("入力してください");
-     		header('Location: profile_edit.html');
-    		exit;
- 		}
+	if (isset($_FILES["user_icon"])) {
+		$user_icon = file_get_contents($_FILES["user_icon"]["tmp_name"]);
+	    $user_icon = str_replace("data:image/jpeg;base64,","",$user_icon);
+	    $user_icon = base64_encode($user_icon);
+	    // var_dump($user_icon);
+	    // exit();
+	}
 		foreach($_POST as $key => $val){
 			$$key=trim(htmlspecialchars($val)); 
 		}
-	if( isset($_FILES["user_icon"]) ){
-	    $user_icon = file_get_contents($_FILES["user_icon"]["tmp_name"]);
-	    $user_icon = str_replace("data:image/jpeg;base64,","",$user_icon);
-	    $user_icon = base64_encode($user_icon);
-  	}
+	$user_id = $_SESSION['user_id'];
+	if (empty($user_id)) {
+		header('Location: ../login/login.html');
+	}
 	try {
 			$pdo = db_connect();
-			$sql = "UPDATE user SET user_name = ?, icon = ? WHERE user_id = ?";
-			$stmt = $pdo->prepare($sql);
-    		$stmt->execute(array($user_name,$user_icon,$user_id));
-
 			if (empty($user_icon)) {
-				$_SESSION["msg"] = 'アイコンが設定されませんでした。';
-				header('Location: profile_edit.php');
+				$sql = "UPDATE user SET user_name = ? WHERE user_id = ?";
+				$sql = $pdo->prepare($sql);
+	    		$sql->execute(array($user_name,$user_id));
+				header('Location: ../post_list.php');
+				var_dump("名前だけ変更しました");
+				exit;
+			}
+			if (isset($_FILES["user_icon"])) {
+			    //var_dump($user_icon);
+				$sql = "UPDATE user SET user_name = ?, icon = ? WHERE user_id = ?";
+				$sql = $pdo->prepare($sql);
+	    		$sql->execute(array($user_name,$user_icon,$user_id));
+				header('Location: ../post_list.php');
+				var_dump("画像と名前を変更しました");
     			exit;
 			}
-
 		} catch (PDOException $e) {
 			exit('データベース接続失敗。'.$e->getMessage());
 		}
-		header('Location: ../post_list.php');
-	}
 ?>
