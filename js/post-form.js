2b2482e6c586874425d71980e66c7b4c06b29a39
ShinyaKobukai <42952490+ShinyaKobukai@users.noreply.github.com>
2020-01-23 11:32:00 +0900

    Add files via upload

--- js/post-form.js
+++ js/post-form.js
@@ -5,16 +5,16 @@ $(function(){
   	//クリックしたときの状態でifが分かれる
   	if( $(this).hasClass("on") ){
   		//post
-  		$("#open").attr("src","img/close.png");
-  		$(this).removeClass("on");
-  		$("#post_form").show();
-  		$("#black-layer").show();
-  	} else {
-  		//close
   		$("#open").attr("src","img/post_icon.png");
-  		$(this).addClass("on");
+  		$(this).removeClass("on");
   		$("#post_form").hide();
   		$("#black-layer").hide();
+  	} else {
+  		//close
+  		$("#open").attr("src","img/close.png");
+  		$(this).addClass("on");
+  		$("#post_form").show();
+  		$("#black-layer").show();
   	}
  });
 });
