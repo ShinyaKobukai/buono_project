9b854a4845fa2359e6457f1b22b7446fd18f7ddb
614KHS <50386471+614KHS@users.noreply.github.com>
2020-01-23 09:39:57 +0900

    Add files via upload

--- search/search_result.php
+++ search/search_result.php
@@ -1,5 +1,9 @@
 <?php
-	include_once("../common/db_connect.php");
+  include_once("../common/db_connect.php");
+  session_start();
+  if (isset($_SESSION['user_id'])) {
+    $login_user = $_SESSION['user_id'];
+  }
 	$pdo = db_connect(); 
 
 	if(empty($_POST['word']) == null){
@@ -38,35 +42,58 @@
 		echo "エラー：" . $e->getMessage();
 	}
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="ja">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/common.css" type="text/css">
+    <link rel="stylesheet" href="../css/search_result.css" type="text/css">
+    <link rel="stylesheet" href="../css/post_form.css" type="text/css">
+    <link rel="stylesheet" href="../css/search_form.css" type="text/css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap">
 	<title>Buono:検索結果</title>
-	<link rel="stylesheet" href="../css/buono.css" />
 </head>
 <body>
-	<header>
-		<h1><a href="../buono_main.php"><img src="../image/logo.png" alt=""><a href="main.php"></a></h1>
-	</header>
-	</br>
-	<div id="result"><img src="../image/loope.png" alt="">　検索ワード　<?php echo "：".$storage; ?></div>
+  <header>
+    <nav id="menu">
+      <ul>
+        <li><a href="../index.php"><i class="fas fa-home"></i>ホーム</a></li>
+      <?php 
+        if (isset($_SESSION['user_id'])) {
+          echo '
+          <li><a href="../edit/profile_edit.html"><i class="fas fa-user-cog"></i>プロフィール編集</a></li>
+          <li><a href="../login/logout.php"><i class="fas fa-sign-out-alt"></i>ログアウト</a></li>
+          ';
+        }elseif (!isset($_SESSION['user_id'])) {
+          echo '
+          <li><a href="../login/register.php"><i class="fas fa-user"></i>アカウント作成</a></li>
+          <li><a href="../login/login.php"><i class="fas fa-sign-in-alt"></i>ログイン</a></li>
+          ';
+        }
+      ?>
+        <li><a href="../post_list.php"><i class="far fa-comments"></i>ポスト</a></li>
+        <li><a id="search_button" type="button"><i class="fas fa-search"></i>検索</a></li>
+      </ul>
+    </nav>
+  </header>
+<main>
+	<div id="result"><img src="../img/loope.png" alt="">　検索ワード　<?php echo "：".$storage; ?></div>
 	</br>
 <?php
 	while ($row = $stmt->fetch()):
 		$food_name = $row['food_name'] ? $row['food_name'] : '(無題)';
 ?>
 		<div id="TimeLine">			
-			<div id="name">
+			<div id="Post_content">
+			<div class="name">
 				<?php 
 					if(empty($row['content']) == null){
-						echo '<div id="icon"><img src="data:image/jpg;base64,' . $row['icon'] . '" width="10%" height="auto"></div>'; 
-						echo $row['user_name'];
+						echo '<div class="icon"><img src="data:image/jpg;base64,' . $row['icon'] . '">'.$row['user_name'].'</div>'; 
 					}
 				?>
 			</div>
-			<div id="Post_content">
-				<p><img src="image/food_menu.png" alt="menu:" width="16" height="16">　<?php echo $row['food_name'] ?></p>
-				<p><img src="image/content.png" alt="review:" width="16" height="16">　<?php echo nl2br($row['content'],false) ?></p>
 					<?php 
 							$post = $row['post_id'];
 							try{
@@ -80,13 +107,35 @@
 						$photo = $line['post_id'];
 						if ($post == $photo) {
 							if(empty($line['data'])==null){
-														echo '<p><img src="data:image/jpeg;base64,' . $line['data'] . '" height="auto" width="45%"></p>';
+														echo '<div class="content_photo">
+																		<img src="data:image/jpeg;base64,' . $line['data'] . '" height="auto" width="45%">
+																	</div>';
 							}
 						}
 					}
 					?>
-				<p><img src="image/balloon.png" alt="場所" width="16" height="16"><?php echo $row['place'] ?></p>
-				<p><img src="image/clock.png" alt="date:" width="16" height="16">　<?php echo $row['post_date'] ?></p>
+				<div class="info">
+					<div class ="food_name"><img src="../img/food_menu.png" alt="menu:" width="16" height="16"><?php echo $row['food_name'] ?></div>
+					<div class="place"><img src="../img/balloon.png" alt="場所" width="16" height="16"><?php echo $row['place'] ?></div>
+					<div class="date"><img src="../img/clock.png" alt="date:" width="16" height="16"><?php echo $row['post_date'] ?></div>
+				</div>
+				<div class="content"><img src="../img/content.png" alt="review:" width="16" height="16">　<?php echo nl2br($row['content'],false) ?></div>
+				<?php 
+          //タグが存在した場合の処理
+          if (isset($row['tag_name'])) {
+            echo '<div class="hash">' .$row['tag_name']. '</div>';
+          } 
+          //ログインの中ユーザーのみ削除と編集のボタンを出す
+          if ($login_user == $row['user_id']) {
+            echo 
+              '<div class="button">
+                <div class="edit"><a href="edit/content_edit.php?content='.$row['content'].'&amp;post_id='.$row['post_id'].'" class="btn-flat-border">編集</a></div>
+                <div class="delete"><a href="edit/content_delete.php?post_id='.$row['post_id'].'">削除</a></div>
+              </div>';
+          }else{
+            echo '<div class="message"><a href="chat/chat_create.php?user_id='.$row['user_id'].'">DM</a></div>';
+          }
+        ?>
 			</div>
 		</div>
 <?php
@@ -117,8 +166,23 @@
 		echo '</div>';
 	?>
 	</div>
-	<div id="main">
-		<a href="../buono_main.php">投稿に戻る</a>
-	</div>
+</main>
+<div id="flex-area-s">
+  <div id="search_form">
+    <p id="top_form_ms">検索</p>
+    <form action="search_result.php" method="post">
+      <p><input type="text" name="food_name" placeholder="料理名かタグを入力してください" size="24" maxlength="20"></p>
+      <p><input type="submit" value="検索"></p>
+    </form>
+  </div>
+</div>
+	<footer>
+		<address>&copy;2019 buono All Rights Reserved.</address>
+	</footer>
+
+  <script src="../js/jquery-2.1.4.min.js"></script>
+  <script src="../js/post-form.js"></script>
+  <script src="../js/search-form.js"></script>
+  <script src="../js/all.js"></script>
 </body>
 </html>
