0ffe0ed989ff9fa714f3d017b6dc8ef172c379ff
614KHS <50386471+614KHS@users.noreply.github.com>
2020-01-23 09:43:14 +0900

    Add files via upload

---
+++ js/search-form.js
@@ -0,0 +1,22 @@
+$(function(){
+	$("#search_form").hide();
+	// $("#black-layer").hide();
+
+  $('#search_button').click(function(){
+  	//クリックしたときの状態でifが分かれる
+
+  	if( $(this).hasClass("on") ){
+
+  		//search
+  		$(this).removeClass("on");
+  		$("#search_form").show();
+  		// $("#black-layer").show();
+  	} else {
+      
+  		//close
+  		$(this).addClass("on");
+  		$("#search_form").hide();
+  		// $("#black-layer").hide();
+  	}
+ });
+});
