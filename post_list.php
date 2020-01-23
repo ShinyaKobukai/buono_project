10e74ad7868fb2b1a94582a092847e10930a5aaa
614KHS <50386471+614KHS@users.noreply.github.com>
2020-01-23 09:42:16 +0900

    Add files via upload

--- post_list.php
+++ post_list.php
@@ -1,11 +1,6 @@
 <?php
   include_once("common/db_connect.php");
   session_start();
-  if(!isset($_SESSION['user_id'])){
-    header("Location: login/login.php");
-    //print("user_idがないでござる");
-    exit();
-  }
   if (isset($_SESSION['user_id'])) {
     $login_user = $_SESSION['user_id'];
   }
@@ -32,19 +27,19 @@
         post.post_date,
         tag.tag_id,
         tag.tag_name
-        FROM
+        FROM 
         user,
         post
         left outer join
         (post_tag join tag on tag.tag_id = post_tag.tag_id)
         on post.post_id = post_tag.post_id
-        WHERE
+        WHERE 
         post.user_id = user.user_id
-        ORDER BY
-        post_date
+        ORDER BY 
+        post_date 
         DESC
-        LIMIT
-        :page, :num"
+        LIMIT 
+        :page, :num"  
     );
     //パラメータの割り当て
     $page = $page * $num;
@@ -65,6 +60,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/common.css" type="text/css">
     <link rel="stylesheet" href="css/post_form.css" type="text/css">
+    <!-- 追加 -->
+    <link rel="stylesheet" href="css/search_form.css" type="text/css">
+    <!-- 　↑　 -->
     <link rel="stylesheet" href="css/post_list.css" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
     <title>Buono -投稿一覧-</title>
@@ -73,12 +71,22 @@
   <header>
     <nav id="menu">
       <ul>
-        <li><a href="index.php"><i class="fas fa-home"></i>Home</a></li>
-        <li><a href="login/register.html"><i class="fas fa-user"></i>Register</a></li>
-        <li><a href="post_list.php"><i class="far fa-comments"></i>Post</a>
-        <li><a href=""><i class="fas fa-search"></i>Search</a></li>
-        <li><a href="edit/profile_edit.php"><i class="fas fa-user-cog"></i>Profile</a></li>
-        <li><a href="login/logout.php"><i class="fas fa-sign-in-alt"></i> <span>Logout</span></a></li>
+        <li><a href="index.php"><i class="fas fa-home"></i>ホーム</a></li>
+      <?php 
+        if (isset($_SESSION['user_id'])) {
+          echo '
+          <li><a href="edit/profile_edit.php"><i class="fas fa-user-cog"></i>プロフィール編集</a></li>
+          <li><a href="login/logout.php"><i class="fas fa-sign-out-alt"></i>ログアウト</a></li>
+          ';
+        }elseif (!isset($_SESSION['user_id'])) {
+          echo '
+          <li><a href="login/register.php"><i class="fas fa-user"></i>アカウント作成</a></li>
+          <li><a href="login/login.php"><i class="fas fa-sign-in-alt"></i>ログイン</a></li>
+          ';
+        }
+      ?>
+        <li><a href="post_list.php"><i class="far fa-comments"></i>ポスト</a></li>
+        <li><a id="search_button" type="button"><i class="fas fa-search"></i>検索</a></li>
       </ul>
     </nav>
   </header>
@@ -87,17 +95,17 @@
   while ($row = $user_stmt->fetch()):
     $food_name = $row['food_name'] ? $row['food_name'] : '(無題)';
 ?>
-    <div id="TimeLine">
+    <div id="TimeLine">     
       <div id="Post_content">
       <div class="name">
-        <?php
+        <?php 
           if(empty($row['content']) == null){
             echo '<div class="icon">
                     <img src="data:image/jpg;base64,' . $row['icon'] . ' "> '.$row['user_name'].'</div>';
           }
         ?>
       </div>
-          <?php
+          <?php 
               $post = $row['post_id'];
               try{
                 $sql = $pdo->prepare("SELECT p.post_id,p.data FROM photo_data AS p,post WHERE :post = p.post_id AND p.post_id = post.post_id ORDER BY post_id DESC");
@@ -116,19 +124,19 @@
           }
           ?>
         <div class="info">
-          <div class ="food_name"><i class="fas fa-utensils"></i><?php echo $row['food_name'] ?></div>
-          <div class="place"><i class="fas fa-location-arrow"></i><?php echo $row['place'] ?></div>
-          <div class="date"><i class="fas fa-clock"></i><?php echo $row['post_date'] ?></div>
+          <div class ="food_name"><img src="img/food_menu.png" alt="menu:" width="16" height="16"><?php echo $row['food_name'] ?></div>
+          <div class="place"><img src="img/balloon.png" alt="場所" width="16" height="16"><?php echo $row['place'] ?></div>
+          <div class="date"><img src="img/clock.png" alt="date:" width="16" height="16"><?php echo $row['post_date'] ?></div>
         </div>
-        <div class="content"><i class="fas fa-comment"></i><?php echo nl2br($row['content'],false) ?></div>
-        <?php
+        <div class="content"><img src="img/content.png" alt="review:" width="16" height="16">　<?php echo nl2br($row['content'],false) ?></div>
+        <?php 
           //タグが存在した場合の処理
           if (isset($row['tag_name'])) {
             echo '<div class="hash">' .$row['tag_name']. '</div>';
-          }
+          } 
           //ログインの中ユーザーのみ削除と編集のボタンを出す
           if ($login_user == $row['user_id']) {
-            echo
+            echo 
               '<div class="button">
                 <div class="edit"><a href="edit/content_edit.php?content='.$row['content'].'&amp;post_id='.$row['post_id'].'" class="btn-flat-border">編集</a></div>
                 <div class="delete"><a href="edit/content_delete.php?post_id='.$row['post_id'].'">削除</a></div>
@@ -157,7 +165,7 @@
   //ページ数を計算
   $max_page = ceil($comments / $num);
   echo '<p>';
-  for ($i=1; $i <= $max_page; $i++) {
+  for ($i=1; $i <= $max_page; $i++) { 
     echo '<a href="post_list.php?page='. $i .'">'.$i. '</a>&nbsp;';
   }
   echo '</p>';
@@ -176,27 +184,41 @@
       <p id="top_form">投稿</p>
       <form action="post_write.php" enctype="multipart/form-data" method="post">
           <p><input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id'];?>"></p></br>
-          <p><img src="img/food_menu.png" alt="menu:" width="16" height="16"><input type="text" name="food_name" placeholder="メニューの名前を入力してください（必須）" size="20" maxlength="20"></p>
+          <p><img src="img/food_menu.png" alt="menu:" width="16" height="16"><input type="text" name="food_name" placeholder="メニューの名前を入力してください（必須）" size="20" maxlength="20"></p> 
         </div>
-          <div id ="review">
+          <div id ="review">  
             <p><img src="img/content.png" alt="review:" width="16" height="16"><textarea name="content" placeholder="感想を入力してください（必須）" rows="4" cols="17"></textarea></p>
           </div>
-          <p><img src="img/balloon.png" alt="場所:" width="16" height="16"><input type="text" name="place" placeholder="場所を入力してください（任意）" size="20" maxlength="20"></p>
+          <p><img src="img/balloon.png" alt="場所:" width="16" height="16"><input type="text" name="place" placeholder="場所を入力してください（任意）" size="20" maxlength="20"></p> 
           <div id ="write">
               <p><input type="file" name="photo[]" id="photo" multiple="multiple" accept="image/jpeg,*.jpg" /></p>
               <input type="hidden" id="base64" name="date" value="" />
               <p><input type="submit" value="投稿する"></p></br>
-          </div>
+          </div>  
         </div>
-      </form>
+      </form> 
   </div>
 </div>
+
+<!-- ↓追加 -->
+<div id="flex-area-s">
+
+  <div id="search_form">
+    <p id="top_form_ms">検索</p>
+    <form action="search/search_result.php" method="post">
+      <p><input type="text" name="food_name" placeholder="料理名かタグを入力してください" size="24" maxlength="20"></p>
+      <p><input type="submit" value="検索"></p>
+    </form>
+  </div>
+</div>
+<!-- 　↑　 -->
   <footer>
     <address>&copy;2019 buono All Rights Reserved.</address>
   </footer>
-
+ 
   <script src="js/jquery-2.1.4.min.js"></script>
   <script src="js/post-form.js"></script>
+  <script src="js/search-form.js"></script>
   <script src="js/all.js"></script>
 </body>
 </html>
