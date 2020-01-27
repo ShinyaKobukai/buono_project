734b489521edee55c6fd0d83716f6ec23083398c
ShinyaKobukai <42952490+ShinyaKobukai@users.noreply.github.com>
2020-01-27 14:16:52 +0900

    0127 kobukai

--- search/search_result.php
+++ search/search_result.php
@@ -4,13 +4,7 @@
   if (isset($_SESSION['user_id'])) {
     $login_user = $_SESSION['user_id'];
   }
-	$pdo = db_connect();
-	// $word = null;
-	// $word = $_POST['word'];
-	// if (empty($word)) {
-	// 	header('Location:../post_list.php');
-	// 	exit();
-	// }
+	$pdo = db_connect(); 
 
 	if(empty($_POST['word']) == null){
 		$input = $_POST['word'];
@@ -54,8 +48,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+       <script src="../js/flickity.pkgd.min.js"></script>
+    <link rel="stylesheet" href="../css/flickity.css">
+    <link rel="stylesheet" href="../css/flickity.min.css">
     <link rel="stylesheet" href="../css/common.css" type="text/css">
-    <link rel="stylesheet" href="../css/search_result.css" type="text/css">
+    <link rel="stylesheet" href="../css/post_list.css" type="text/css">
     <link rel="stylesheet" href="../css/post_form.css" type="text/css">
     <link rel="stylesheet" href="../css/search_form.css" type="text/css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap">
@@ -66,64 +63,89 @@
     <nav id="menu">
       <ul>
         <li><a href="../index.php"><i class="fas fa-home"></i>Home</a></li>
-        <li><a href="../login/register.php"><i class="fas fa-user"></i>Register</a></li>
-        <li><a href="../post/post_list.php"><i class="far fa-comments"></i>Post</a>
-        <li><a id="search_button"><i class="fas fa-search"></i>Search</a></li>
-        <li><a href="../edit/profile_edit.php"><i class="fas fa-user-cog"></i>Profile</a></li>
-        <li><a href="../login/logout.php"><i class="fas fa-sign-in-alt"></i> <span>Logout</span></a></li>
+      <?php 
+        if (isset($_SESSION['user_id'])) {
+          echo '
+          <li><a href="../edit/profile_edit.html"><i class="fas fa-user-cog"></i>Edit</a></li>
+          <li><a href="../login/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
+          ';
+        }elseif (!isset($_SESSION['user_id'])) {
+          echo '
+          <li><a href="../login/register.html"><i class="fas fa-user"></i>Register</a></li>
+          <li><a href="../login/login.php"><i class="fas fa-sign-in-alt"></i>Login</a></li>
+          ';
+        }
+      ?>
+        <li><a href="../post_list.php"><i class="far fa-comments"></i>Post</a></li>
+        <li><a id="search_button" href="#"><i class="fas fa-search"></i>Search</a></li></li>
       </ul>
     </nav>
   </header>
 <main>
-	<div id="result"><img src="../img/loope.png" alt="">　検索ワード　<?php echo "：".$storage; ?></div>
+	<div id="result"><i class="fas fa-search"></i>　検索ワード<?php echo "：".$storage; ?></div>
 	</br>
 <?php
 	while ($row = $stmt->fetch()):
 		$food_name = $row['food_name'] ? $row['food_name'] : '(無題)';
 ?>
-		<div id="TimeLine">
+		<div id="TimeLine">			
 			<div id="Post_content">
 			<div class="name">
-				<?php
+				<?php 
 					if(empty($row['content']) == null){
-						echo '<div class="icon"><img src="data:image/jpg;base64,' . $row['icon'] . '">'.$row['user_name'].'</div>';
+						echo '<div class="icon">
+			                    <img src="data:image/jpg;base64,' . $row['icon'] . ' ">
+			                    <div class="icon_name"> '.$row['user_name'].'</div>
+			                  </div>';
 					}
 				?>
 			</div>
-					<?php
-							$post = $row['post_id'];
-							try{
-								$sql = $pdo->prepare("SELECT p.post_id,p.data FROM photo_data AS p,post WHERE :post = p.post_id AND p.post_id = post.post_id ORDER BY post_id DESC");
-								$sql->bindParam(':post',$post,PDO::PARAM_INT);
-								$sql->execute();
-							}catch(PDOException $e){
-								echo "エラー：" . $e->getMessage();
-							}
-					while ($line = $sql->fetch()) {
-						$photo = $line['post_id'];
-						if ($post == $photo) {
-							if(empty($line['data'])==null){
-														echo '<div class="content_photo">
-																		<img src="data:image/jpeg;base64,' . $line['data'] . '" height="auto" width="45%">
-																	</div>';
-							}
-						}
-					}
-					?>
+			<?php 
+		        $post = $row['post_id'];
+		        try{
+		          $sql = $pdo->prepare("SELECT p.post_id,p.data FROM photo_data AS p,post WHERE :post = p.post_id AND p.post_id = post.post_id ORDER BY post_id DESC");
+		          $sql->bindParam(':post',$post,PDO::PARAM_INT);
+		          $sql->execute();
+		        }catch(PDOException $e){
+		          echo "エラー：" . $e->getMessage();
+		        }
+
+		        $result_data = $sql->fetchAll(PDO::FETCH_ASSOC);
+		        //var_dump($result_data);
+		        ?>
+		        <?php if(count($result_data)):?>
+		        <?php if ($post == $result_data[0]['post_id']):?>
+		          <div class="photo_space">
+		            <div class="content_photo flickity-syncer">
+		              <?php
+
+		              $output_html = '';
+		              for($slide_num=0; $slide_num<count($result_data); $slide_num++){
+		                $output_html .= '<img src="data:image/jpeg;base64,' . $result_data[$slide_num]['data'] . '" height = 250px;/>';
+		              }
+		              echo $output_html;
+		              $output_html = '';
+		              ?>
+		            </div>
+		          </div>
+		        <?php endif;?>
+		        <?php endif;?>
+
 				<div class="info">
-					<div class ="food_name"><img src="../img/food_menu.png" alt="menu:" width="16" height="16"><?php echo $row['food_name'] ?></div>
-					<div class="place"><img src="../img/balloon.png" alt="場所" width="16" height="16"><?php echo $row['place'] ?></div>
-					<div class="date"><img src="../img/clock.png" alt="date:" width="16" height="16"><?php echo $row['post_date'] ?></div>
-				</div>
-				<div class="content"><img src="../img/content.png" alt="review:" width="16" height="16">　<?php echo nl2br($row['content'],false) ?></div>
-				<?php
+		          <div class="btn_space"></div>
+		          <div class ="food_name"><i class="fas fa-utensils"></i>　<?php echo $row['food_name'] ?></div>
+		          <div class="content"><i class="fas fa-comment"></i>　<?php echo nl2br($row['content'],false) ?></div>
+		          <div class="place"><i class="fas fa-location-arrow"></i>　<?php echo $row['place'] ?></div>
+		          <div class="date"><i class="fas fa-clock"></i>　<?php echo $row['post_date'] ?></div>
+		        </div>
+		 <?php 
           //タグが存在した場合の処理
           if (isset($row['tag_name'])) {
-            echo '<div class="hash">' .$row['tag_name']. '</div>';
-          }
+            echo '<div class="hash"><i class="fas fa-tag"></i>　' .$row['tag_name']. '</div>';
+          } 
           //ログインの中ユーザーのみ削除と編集のボタンを出す
           if ($login_user == $row['user_id']) {
-            echo
+            echo 
               '<div class="button">
                 <div class="edit"><a href="edit/content_edit.php?content='.$row['content'].'&amp;post_id='.$row['post_id'].'" class="btn-flat-border">編集</a></div>
                 <div class="delete"><a href="edit/content_delete.php?post_id='.$row['post_id'].'">削除</a></div>
@@ -151,12 +173,12 @@
 	$comments = $stmt->fetchColumn();
 	//ページ数を計算
 	$max_page = ceil($comments / $num);
-		echo '<div id="page">';
-		for ($i=1; $i <= $max_page; $i++) {
+		echo '<div id="page_num">';
+		for ($i=1; $i <= $max_page; $i++) { 
 			if(empty($_POST['word']) == null){
-				echo '<form action="search_result.php?page='. $i .'" name="form'.$i.'" method="post"><input type="hidden" name="word" value="'.$_POST['word'].'" /><p><a href="javascript:form'.$i.'.submit()">'.$i. '</a></p></form>&nbsp;';
+				echo '<form action="search_result.php?page='. $i .'" name="form'.$i.'" method="post"><input type="hidden" name="word" value="'.$_POST['word'].'" /><a href="javascript:form'.$i.'.submit()">'.$i. '</a></form>&nbsp;';
 			}else if (empty($_POST['food_name']) == null) {
-				echo '<form action="search_result.php?page='. $i .'" name="form'.$i.'" method="post"><input type="hidden" name="word" value="'.$_POST['food_name'].'" /><p><a href="javascript:form'.$i.'.submit()">'.$i. '</a></p></form>&nbsp;';
+				echo '<form action="search_result.php?page='. $i .'" name="form'.$i.'" method="post"><input type="hidden" name="word" value="'.$_POST['food_name'].'" /><a href="javascript:form'.$i.'.submit()">'.$i. '</a></form>&nbsp;';
 			}
 		}
 		echo '</div>';
@@ -175,10 +197,12 @@
 	<footer>
 		<address>&copy;2019 buono All Rights Reserved.</address>
 	</footer>
-
   <script src="../js/jquery-2.1.4.min.js"></script>
   <script src="../js/post-form.js"></script>
-  <script src="../js/search-form.js"></script>
   <script src="../js/all.js"></script>
+  <script src="../js/search-form.js"></script>
+  <script src="../js/photo_rule.js"></script>
+  <script src="../js/post_slide.js"></script>
+  <script src="../js/jquery-2.1.4.min.js"></script>
 </body>
 </html>
