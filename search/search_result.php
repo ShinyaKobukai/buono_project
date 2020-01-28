c1ff59ced7a4cb98c0b8661dd88e315ac99caa4e
ShinyaKobukai <42952490+ShinyaKobukai@users.noreply.github.com>
2020-01-28 10:28:20 +0900

    insert favicon

--- search/search_result.php
+++ search/search_result.php
@@ -48,7 +48,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-       <script src="../js/flickity.pkgd.min.js"></script>
+	<script src="../js/flickity.pkgd.min.js"></script>
+	<link rel="icon" href="img/favicon.ico">
     <link rel="stylesheet" href="../css/flickity.css">
     <link rel="stylesheet" href="../css/flickity.min.css">
     <link rel="stylesheet" href="../css/common.css" type="text/css">
