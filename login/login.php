9e317a11453de88143ac4679fbcd9163061e0fee
ShinyaKobukai <42952490+ShinyaKobukai@users.noreply.github.com>
2020-01-28 13:23:57 +0900

    Add files via upload

--- login/login.php
+++ login/login.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="../img/favicon.ico">
     <link rel="stylesheet" href="../css/common.css" type="text/css">
     <link rel="stylesheet" href="../css/register.css" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
