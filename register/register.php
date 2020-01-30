02a282530211c6e4fb0fb17e886661ccdf070665
YATO <42953961+AckerStewie@users.noreply.github.com>
2020-01-30 14:16:51 +0900

    Update register.php
    ディレクトリ指定を「php」から「post」に変更

--- register/register.php
+++ register/register.php
@@ -22,7 +22,7 @@
       <?php
         //menu();
         if(!empty($_SESSION['user_id'])){
-          echo '<li><a href="../php/post_list.php"><i class="far fa-comments"></i>Post</a></li>
+          echo '<li><a href="../post/post_list.php"><i class="far fa-comments"></i>Post</a></li>
                 <li><a href="../login/logout.php"><i class="fas fa-sign-in-alt"></i> <span>Logout</span></a></li>
                 ';
         }else{
