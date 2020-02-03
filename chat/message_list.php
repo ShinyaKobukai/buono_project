41a040164c28ca78641b259ecd91821a3f9ceaa2
ShinyaKobukai <42952490+ShinyaKobukai@users.noreply.github.com>
2020-02-03 12:31:13 +0900

    button flat design

--- chat/message_list.php
+++ chat/message_list.php
@@ -55,8 +55,8 @@
 	<header>
 		<nav id="menu">
 	      <ul>
-	        <li><a href="../index.php"><i class="fas fa-home"></i>Home</a></li>
-	        <li><a href="../post/post_list.php"><i class="far fa-comments"></i>Post</a></li>
+	        <li><a href="../index.php"><i class="fas fa-home"></i><span>Home</span></a></li>
+	        <li><a href="../post/post_list.php"><i class="far fa-comments"></i><span>Post</span></a></li>
 	        <li><a href="../login/logout.php"><i class="fas fa-sign-in-alt"></i><span>Logout</span></a></li>
 	      </ul>
     	</nav>
